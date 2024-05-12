a589de5263ce8b5a21eab900ce24320bb9d463c1
haarsh157 <harshrange123@gmail.com>
2024-05-12 15:27:44 +0530

    managing members done

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer" | "invite" | "serverSettings" | "leaveServer" | "deleteServer";
+export type ModalType = "createServer" | "invite" | "serverSettings" | "leaveServer" | "deleteServer" | "members";
 
 interface ModalData {
   server?: Server;
