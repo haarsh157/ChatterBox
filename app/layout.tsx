2acc662164e61364ffe56a1d75d9a3703b0c4671
haarsh157 <harshrange123@gmail.com>
2024-05-14 12:06:38 +0530

    socket-io setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+
 import { ThemeProvider } from "@/components/provider/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/components/provider/modal-provider";
+import { SocketProvider } from "@/components/provider/socket-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -36,8 +38,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
