ba9348c005b459ce023e7f92bfa58575e0d504f1
haarsh157 <harshrange123@gmail.com>
2024-05-14 13:27:41 +0530

    messages api

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
@@ -46,6 +47,12 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
           name={channel.name}
           type="channel"
           bgImage={profile.bgImage}
+        />
+        <ChatInput
+          name={channel.name}
+          type="channel"
+          apiUrl="/api/socket/messages"
+          query={{ channelId: channel.id, serverId: channel.serverId }}
         />
       </div>
     </div>
