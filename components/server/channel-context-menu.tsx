24954ae5161eb90a848168f02e2dc6eb7e007a02
haarsh157 <harshrange123@gmail.com>
2024-05-16 09:29:06 +0530

    channel context menu changes

--- components/server/channel-context-menu.tsx
+++ components/server/channel-context-menu.tsx
@@ -39,16 +39,16 @@ export const ChannelContextMenu = ({
       <ContextMenuContent className="w-64 border-none bg-black">
         {channelName !== "general" && role !== MemberRole.GUEST && (
           <>
-            <ContextMenuItem inset onClick={(e) => onAction(e, "editChannel")}>
-              <p>Edit Message</p>
-              <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-indigo-600 dark:text-zinc-400 dark:hover:text-indigo-500 transition" />
+            <ContextMenuItem inset onClick={(e) => onAction(e, "editChannel")} className="flex justify-between items-center">
+              <p>Edit Channel</p>
+              <Edit className="group-hover:block w-4 h-4 text-zinc-500 hover:text-indigo-600 dark:text-zinc-400 dark:hover:text-indigo-500 transition" />
             </ContextMenuItem>
             <ContextMenuItem
               inset
-              onClick={(e) => onAction(e, "deleteChannel")}
+              onClick={(e) => onAction(e, "deleteChannel")} className="flex justify-between items-center"
             >
               <p>Delete Channel</p>
-              <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-rose-500 dark:text-zinc-400 dark:hover:text-rose-500 transition" />
+              <Trash className="group-hover:block w-4 h-4 text-zinc-500 hover:text-rose-500 dark:text-zinc-400 dark:hover:text-rose-500 transition" />
             </ContextMenuItem>
           </>
         )}
